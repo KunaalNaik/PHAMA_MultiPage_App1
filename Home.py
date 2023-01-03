707c5b6ba040373d22a0a409acdd30c7999273f4
Kunaal <fxexcel@gmail.com>
2023-01-03 19:35:58 +0530

    updated home

--- Home.py
+++ Home.py
@@ -7,23 +7,25 @@ st.set_page_config(
 )
 
 st.title("Project PHAMA - Clinical Trails")
-st.markdown(f'This application allows you download Clinical trials raw data.  \n'
-            f'The raw data is richer than what you can download from the website directly.  \n'
-            f'\n Apart from downloading raw data, it also helps generate analytical views and compares current vs historical data.  \n'
-            f'## Tools\n'
-            f'TBD  \n'
-            f'### 1/ Download Raw Data\n'
-            f'TBD  \n'
-            f'### 2/ Landscape\n'
-            f'TBD  \n'
-            f'### 3/ Geography\n'
-            f'TBD  \n'
-            f'### 4/ Launch Estimates\n'
-            f'TBD  \n'
-            f'### 5/ KOL\n'
-            f'TBD  \n'
-            f'### 6/ Data Compare\n'
-            )
+st.write("""
+This application allows you download Clinical trials raw data. 
+The raw data is richer than what you can download from the website directly.
+Apart from downloading raw data, it also helps generate analytical views and compares current vs historical data.
+
+## Tools
+TBD
+### 1/ Download Raw Data
+TBD
+### 2/ Landscape
+TBD
+### 3/ Geography
+TBD
+### 4/ Launch Estimates
+TBD
+### 5/ KOL
+TBD
+### 6/ Data Compare
+""")
 
 
 
